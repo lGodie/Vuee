2923e0a98a37abf72475ba14abc649d9422d90a5
DIEGO ALEJANDRO ZAPATA VANEGAS <diegozapata1345@gmail.com>
2019-08-08 16:53:58 -0500

    Vue app

--- App.vue
+++ App.vue
@@ -1,29 +1,36 @@
 <template>
-  <div>
-    <table>
-      <tbody>
-        <tr v-for=" MostrarNoticias in Noticias" v-bind:key="MostrarNoticias.id">
-          <td>{{ MostrarNoticias.userId }}</td>
-          <td>{{ MostrarNoticias.id }}</td>
-          <td>{{ MostrarNoticias.title }}</td>
-          <td>{{ MostrarNoticias.body }}</td>
-        </tr>
-      </tbody>
-    </table>
+  <div id="App">
+    <h1 v-show="HideNews">Noticias</h1>
+    <div v-show="HideNews">
+      <button @click="CloseNews()">Abrir</button>
+      <div v-for=" MostrarNoticias in Noticias" v-bind:key="MostrarNoticias.id" class="news">
+        <h1>{{ MostrarNoticias.title }}</h1>
+        <p>{{ MostrarNoticias.body }}</p>
+      </div>
+    </div>
+    <button @click="ShowNews()">Guardar</button>
+
+    <div v-show="!HideNews">
+      <Noticias1 ref="Noticias1" />
+    </div>
+    <div></div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import Noticias1 from "./Noticias.vue";
 
 export default {
+  components: { Noticias1 },
   data() {
     return {
       Hello: "Hello world",
-      Noticias: ""
+      Noticias: "",
+      HideNews: false
     };
   },
-//Comment
+  //Comment
   mounted() {
     this.NewFunc();
     this.getConsumir();
@@ -40,10 +47,27 @@ export default {
         console.log(Response.data);
         this.Noticias = Response.data;
       });
+    },
+
+    OpenNews() {
+      this.HideNews = true;
+    },
+
+    ShowNews() {
+      this.HideNews = true;
+    },
+
+    CloseNews() {
+      this.HideNews = false;
     }
   }
 };
 </script>
 
 <style>
+.news {
+  border: 1px solid #999;
+  padding: 20px;
+  margin-bottom: 50px;
+}
 </style>
---
+++ Noticias.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Formulario</h1>
+
+    <form>
+      <p>
+        Nombre:
+        <input type="text" name="nombre" size="40" />
+      </p>
+      <p>
+        Año de nacimiento:
+        <input type="number" name="nacido" min="1900" />
+      </p>
+      <p>
+        Sexo:
+        <input type="radio" name="hm" value="h" /> Hombre
+        <input type="radio" name="hm" value="m" /> Mujer
+      </p>
+      <p>
+        <button @click="GuardarFunc()">Guardar</button>
+        <button @click="CloseFunc()">Cancelar</button>
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      HideNews: false
+    };
+  },
+  methods: {
+    GuardarFunc() {
+      this.HideNews = true;
+    },
+
+    CloseFunc() {
+      this.HideNews = false;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
